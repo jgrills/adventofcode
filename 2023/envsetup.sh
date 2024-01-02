74769c920bb5c46fd91194b3f281a3d701acc76b
Jeff Grills <jeff.grills@gmail.com>
2024-01-01 19:17:38 -0600

    Add some helper files

---
+++ 2023/envsetup.sh
@@ -0,0 +1,7 @@
+
+c() { clear; }
+b() { cargo build; }
+re() { target/debug/app example;  }
+re1() { target/debug/app example1;  }
+re2() { target/debug/app example2; }
+ri() { target/debug/app input;  }
