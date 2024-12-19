8cb6dce687515a0e79a492fb2c709b9c968acfee
Jeff Grills <jeff.grills@gmail.com>
2024-12-18 23:05:00 -0600

    Chromebook file

--- 2023/envsetup.sh
+++ 2023/envsetup.sh
@@ -1,12 +1,18 @@
+<<<<<<< HEAD
 
 export LESS=R
 
 c() { clear; }
 b() { cargo --color always build; }
+=======
+c() { clear; }
+b() { cargo build; }
+>>>>>>> fa9437d (Chromebook file)
 re() { target/debug/app example;  }
 re1() { target/debug/app example1;  }
 re2() { target/debug/app example2; }
 ri() { target/debug/app input;  }
+<<<<<<< HEAD
 
 wb() {
     inotifywait -q -m -e close_write src/main.rs |
@@ -18,3 +24,5 @@ wb() {
         fi
     done
 }
+=======
+>>>>>>> fa9437d (Chromebook file)
